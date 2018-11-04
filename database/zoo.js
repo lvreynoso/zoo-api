b5b6a4328dfaeab781d250552d343017baf59147
LinnierGames <esericksanc@gmail.com>
2018-11-04 14:29:21 -0800

    Refactor Routes and Database into Modules

---
+++ database/zoo.js
@@ -0,0 +1,11 @@
+module.exports = {
+    '0001': {
+        name: 'Human',
+        species: 'Homo Sapiens',
+    },
+    '0002':
+    {
+        name: 'Wolf',
+        species: 'Canis Lupus',
+    }
+}
---
+++ routes/animals.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var zoo = require('./../database/zoo')
+
+// get a list of objects
+router.get(`/`, (req, res) => {
+    res.status(200).send(zoo);
+})
+
+// get an object by id
+router.get(`/:id`, (req, res) => {
+    animalID = req.params.id
+    res.status(200).send(zoo[animalID])
+})
+
+// add an object to the list
+router.post(`/`, (req, res) => {
+    animalID = req.body.id
+    animal = {
+        name: req.body.name,
+        species: req.body.species
+    }
+    zoo[animalID] = animal
+    res.status(200).send(zoo[animalID])
+})
+
+// update an object on the list
+router.put(`/:id`, (req, res) => {
+    animalID = req.params.id
+    animal = {
+        name: req.body.name,
+        species: req.body.species
+    }
+    zoo[animalID] = animal
+    res.status(200).send(zoo[animalID])
+})
+
+// delete an object on the list
+router.delete(`/:id`, (req, res) => {
+    animalID = req.params.id
+    deletedAnimal = zoo[animalID]
+    delete zoo[animalID]
+    res.status(200).send(deletedAnimal)
+})
+
+module.exports = router
